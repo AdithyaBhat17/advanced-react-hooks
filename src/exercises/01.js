11c276652c52faeb0f48f8be726188e738d091a7
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-05-13 01:00:25 +0530

    :tada: exercise 1 done with useReducer

--- src/exercises/01.js
+++ src/exercises/01.js
@@ -1,17 +1,29 @@
 // useReducer: simple Counter
 import React from 'react'
 
+function countReducer(state, action) {
+  switch(action.type) {
+    case 'increment': 
+      return {count: state.count + 1}
+    default: {
+      throw new Error(action.type)
+    }
+  }
+}
+
 function Counter({step = 1, initialCount = 0}) {
   // 🐨 replace React.useState with React.useReducer.
-  const [count, setCount] = React.useState(initialCount)
+  const [state, dispatch] = React.useReducer(countReducer, {
+    count: initialCount
+  })
   // 🦉 you can inline your reducer function as the first argument to
   // React.useReducer (this is especially useful if the reducer function is
   // configurable via props), but more often you'll find people place the
   // reducer function outside the component.
-
+  const { count } = state
   // 💰 you can write your reducer so you don't have to make any changes
   // to the next two lines of code!
-  const increment = () => setCount(c => c + step)
+  const increment = () => dispatch({type: 'increment'})
   return <button onClick={increment}>{count}</button>
 }
 
